323a656b5586631152f1438f3b398d37f78f8b85
Filip Olesinski <filip.olesinski21@gmail.com>
2023-01-15 20:27:58 +0100

    improved learning procedure by adding polynolmial features to training and testing datasets, new approximate metrics are [score: 0.8, MSE: 2.5, MAE: 1.2]

--- data_setup.py
+++ data_setup.py
@@ -12,20 +12,12 @@ for file in os.listdir(current_dir):
     if os.path.isfile(f) and '.csv' in os.path.basename(f):
         files_to_filter.append(os.path.basename(f))
 
-# checking if the final data .csv file already exists, if so delete all its content. Otherwise, create it.
-if "reviews_full.csv" in files_to_filter:
-    with open("reviews_full.csv", 'r+') as file:
-        file.truncate(0)
-else:
-    final_data_file = open("reviews_full.csv", "x")
-
 # connecting data from all .csv files into one. We are taking only english reviews into consideration.
 for file_csv in files_to_filter:
-    unfiltered_reviews = pd.read_csv(file_csv, names=['id', 'title', 'date', 'score', 'review', 'english'],
-                                     index_col=False)
+    unfiltered_reviews = pd.read_csv(file_csv, names=['id', 'title', 'date', 'score', 'review', 'english'])
     unfiltered_reviews.dropna(inplace=True)
     unfiltered_reviews = unfiltered_reviews[unfiltered_reviews.english == True]
     data = pd.concat([data, unfiltered_reviews])
 
-# saving our final data data frame into .csv file
+# saving our final data data frame into .csv file.
 data.to_csv('reviews_full.csv', index=False)
--- learning.py
+++ learning.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import numpy as np
-import random
 import nltk
 
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
 
 tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
+poly = PolynomialFeatures(degree=6)
 
 data_reg = pd.read_csv('check\\reviews_analyzed.csv', header=0, usecols=['review', 'semantic', 'score'])
 
@@ -18,12 +19,15 @@ y = np.array(data_reg['score']).reshape(data_reg.shape[0], 1)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
+X_train = poly.fit_transform(X_train)
+X_test = poly.fit_transform(X_test)
+
 regression = LinearRegression()
 regression.fit(X_train, y_train)
 
 predicted = regression.predict(X_test)
 predicted = [np.sum(np.round(x)) for x in predicted]
-dumb_predict = [random.randint(0, 10) for x in range(0, len(y_test))]
+dumb_predict = [round(data_reg['score'].mean()) for x in range(0, len(y_test))]
 
 metrics_df = pd.DataFrame(data={'metric': ['MSE', 'MAE', 'Score'],
                                 'regression_model': [mean_squared_error(y_test, predicted),
